3c97594e0eb1d77e3fabd524d6065147b2f6d83e
Shubham Avasthi <shubhamavasthi1@gmail.com>
2021-04-26 23:54:13 +0530

    Updated files and added results

--- residence-time.py
+++ residence-time.py
@@ -5,6 +5,8 @@ import numpy as np
 from math import inf, sqrt
 from sklearn.linear_model import LinearRegression
 
+from csv import DictWriter
+
 argument_parser = argparse.ArgumentParser(description = 'Molecular Dynamics Residence Time')
 argument_parser.add_argument('data_file', type = str, help = 'Data file')
 argument_parser.add_argument('dump_file', type = str, help = 'Dump file')
@@ -44,73 +46,83 @@ class AverageCoords:
 			self.coords[i] = (self.coords[i] * self.num + coords[i]) / (self.num + 1)
 		self.num += 1
 
-atom_id_to_mol_id = {}
-mols_continuously_remained_adsorbed = set()
-with open(data_file, newline = '') as datafile:
-	for line in datafile:
-		if line == 'Atoms\n':
-			break
-	datafile.readline()
-	for line in datafile:
-		if line == '\n':
-			break
-		atom_id, mol_id, _, _, _, _, _ = line.split()
-		atom_id = int(atom_id)
-		mol_id = int(mol_id)
-		atom_id_to_mol_id[atom_id] = mol_id
-		if is_adsorbate_atom(atom_id):
-			mols_continuously_remained_adsorbed.add(mol_id)
 
-initially_adsorbed_mols = -1		# Will be initialized after the first timestep data gets processed
-auto_correlation = []
-timesteps = []
-with open(dump_file, newline = '') as dumpfile:
-	while dumpfile.readline():
-		timestep = int(dumpfile.readline().strip())
-		dumpfile.readline()
-		num_atoms = int(dumpfile.readline().strip())
+with open('out.csv', 'w', newline='') as outfile:
+	d = DictWriter(outfile, fieldnames = [100 * i for i in range(1001)])
 
-		for _ in range(5):
-			dumpfile.readline()
-		
-		adsorbent_atoms_coords = []
-		adsorbate_mols_avg_coords = {}
-		for i in range(num_atoms):
-			coords = [0] * 3
-			atom_id, _, coords[0], coords[1], coords[2], _, _, _ = dumpfile.readline().split()
-			atom_id = int(atom_id)
-			for j in range(3):
-				coords[j] = float(coords[j])
+	for t0 in range(0, 100001, 100):
+		atom_id_to_mol_id = {}
+		mols_continuously_remained_adsorbed = set()
+		with open(data_file, newline = '') as datafile:
+			for line in datafile:
+				if line == 'Atoms\n':
+					break
+			datafile.readline()
+			for line in datafile:
+				if line == '\n':
+					break
+				atom_id, mol_id, _, _, _, _, _ = line.split()
+				atom_id = int(atom_id)
+				mol_id = int(mol_id)
+				atom_id_to_mol_id[atom_id] = mol_id
+				if is_adsorbate_atom(atom_id):
+					mols_continuously_remained_adsorbed.add(mol_id)
 
-			if is_adsorbent_atom(atom_id):
-				adsorbent_atoms_coords.append(coords)
-			
-			if is_adsorbate_atom(atom_id):
-				mol_id = atom_id_to_mol_id[atom_id]
-				if mol_id in adsorbate_mols_avg_coords:
-					adsorbate_mols_avg_coords[mol_id].add_contribution(coords)
-				else:
-					adsorbate_mols_avg_coords[mol_id] = AverageCoords(coords)
+		initially_adsorbed_mols = -1		# Will be initialized after the first timestep data gets processed
+		auto_correlation = []
+		timesteps = []
+		with open(dump_file, newline = '') as dumpfile:
+			while dumpfile.readline():
+				timestep = int(dumpfile.readline().strip())
+				dumpfile.readline()
+				num_atoms = int(dumpfile.readline().strip())
 
-		new_adsorbed_set = set()
-		for adsorbent_coords in adsorbent_atoms_coords:
-			min_dist = inf
-			closest_adsorbate_mol_id = -1
-			for adsorbate_mol_id, adsorbate_avg_coords in adsorbate_mols_avg_coords.items():
-				dist = distance(adsorbent_coords, adsorbate_avg_coords.coords)
-				if dist < min_dist:
-					min_dist = dist
-					closest_adsorbate_mol_id = adsorbate_mol_id
-			if closest_adsorbate_mol_id in mols_continuously_remained_adsorbed:
-				new_adsorbed_set.add(closest_adsorbate_mol_id)
-		mols_continuously_remained_adsorbed = new_adsorbed_set
+				for _ in range(5):
+					dumpfile.readline()
+				
+				adsorbent_atoms_coords = []
+				adsorbate_mols_avg_coords = {}
+				for i in range(num_atoms):
+					coords = [0] * 3
+					atom_id, _, coords[0], coords[1], coords[2], _, _, _ = dumpfile.readline().split()
+					atom_id = int(atom_id)
+					for j in range(3):
+						coords[j] = float(coords[j])
 
-		if initially_adsorbed_mols == -1:
-			initially_adsorbed_mols = len(mols_continuously_remained_adsorbed)
-		
-		auto_correlation.append(len(mols_continuously_remained_adsorbed) / initially_adsorbed_mols)
-		timesteps.append(timestep)
+					if is_adsorbent_atom(atom_id):
+						adsorbent_atoms_coords.append(coords)
+					
+					if is_adsorbate_atom(atom_id):
+						mol_id = atom_id_to_mol_id[atom_id]
+						if mol_id in adsorbate_mols_avg_coords:
+							adsorbate_mols_avg_coords[mol_id].add_contribution(coords)
+						else:
+							adsorbate_mols_avg_coords[mol_id] = AverageCoords(coords)
+				if timestep < t0:
+					continue
 
-reg = LinearRegression().fit(np.asarray(timesteps).reshape(-1, 1), np.log(auto_correlation).reshape(-1, 1))
+				new_adsorbed_set = set()
+				for adsorbent_coords in adsorbent_atoms_coords:
+					min_dist = inf
+					closest_adsorbate_mol_id = -1
+					for adsorbate_mol_id, adsorbate_avg_coords in adsorbate_mols_avg_coords.items():
+						dist = distance(adsorbent_coords, adsorbate_avg_coords.coords)
+						if dist < min_dist:
+							min_dist = dist
+							closest_adsorbate_mol_id = adsorbate_mol_id
+					if closest_adsorbate_mol_id in mols_continuously_remained_adsorbed:
+						new_adsorbed_set.add(closest_adsorbate_mol_id)
+				mols_continuously_remained_adsorbed = new_adsorbed_set
 
-print(f"Calculated residence time (in timesteps):", -1 / reg.coef_[0, 0])
+				if initially_adsorbed_mols == -1:
+					initially_adsorbed_mols = len(mols_continuously_remained_adsorbed)
+				
+				auto_correlation.append(len(mols_continuously_remained_adsorbed) / initially_adsorbed_mols)
+				timesteps.append(timestep)
+
+		d.writerow({t0 + 100 * i: auto_correlation[i] for i in range(len(auto_correlation))})
+
+		while auto_correlation and auto_correlation[-1] == 0:
+			auto_correlation.pop()
+			timesteps.pop()
+		d.writerow({t0 + 100 * i: -1 / LinearRegression().fit(np.asarray(timesteps[:i+1]).reshape(-1, 1), np.log(auto_correlation[:i+1]).reshape(-1, 1)).coef_[0, 0] for i in range(len(auto_correlation))})
